c44e1de0239af871f972b203fa646cd373134be5
Jacob <jacobhart1227@gmail.com>
2021-11-28 10:29:30 -0700

    Add edit/delete to admin page

--- front-end/src/views/Admin.vue
+++ front-end/src/views/Admin.vue
@@ -1,21 +1,38 @@
 <template>
 <div>
-  <h3 class="text-light text-center mb-4 mt-3">Add a product</h3>
-    <div class="row justify-content-center">
-      <div class="col-6">
-        <label class="text-light font-weight-normal">Product name</label>
-        <input v-model="name" placeholder="Enter the product's name" class="form-control bg-dark text-light border-light"/>
-        <label class="mt-3 text-light font-weight-normal">Does it have a dark theme?</label>
-        <select v-model="dark" class="form-control bg-dark text-light border-light" name="dark">
-          <option value="1">Yes!</option>
-          <option value="2">Yes, but...</option>
-          <option value="0">No</option>
-        </select>
-        <label class="mt-3 text-light font-weight-normal">Context</label>
-        <textarea v-model="context" placeholder="Enter a description" rows="3" class="form-control bg-dark text-light border-light"/>
-        <button @click="addProduct" id="submit" class="mt-4 btn btn-lg text-light border-light">Add to database</button>
-      </div>
+  <div class="row justify-content-center">
+    <div class="col-4 mr-3">
+      <h3 class="text-light text-center mb-3">Add a product</h3>
+      <label class="text-light font-weight-normal">Product name</label>
+      <input v-model="addName" placeholder="Enter the product's name" class="form-control bg-dark text-light border-light"/>
+      <label class="mt-3 text-light font-weight-normal">Does it have a dark theme?</label>
+      <select v-model="addDark" class="form-control bg-dark text-light border-light" name="dark">
+        <option value="1">Yes!</option>
+        <option value="2">Yes, but...</option>
+        <option value="0">No</option>
+      </select>
+      <label class="mt-3 text-light font-weight-normal">Context</label>
+      <textarea v-model="addContext" placeholder="Enter a description" rows="3" class="form-control bg-dark text-light border-light"/>
+      <button @click="addProduct" id="submit" class="mt-3 btn btn-lg text-light border-light">Add product</button>
     </div>
+    <div class="col-4 ml-3  ">
+      <h3 class="text-light text-center mb-3">Edit or delete a product</h3>
+      <label class="text-light font-weight-normal">Product name</label>
+      <select v-model="editName" class="form-control bg-dark text-light border-light" name="dark">
+        <option v-for="product in products" :key="product._id">{{product.name}}</option>
+      </select>
+      <label class="mt-3 text-light font-weight-normal">Does it have a dark theme?</label>
+      <select v-model="editDark" class="form-control bg-dark text-light border-light" name="dark">
+        <option value="1">Yes!</option>
+        <option value="2">Yes, but...</option>
+        <option value="0">No</option>
+      </select>
+      <label class="mt-3 text-light font-weight-normal">Context</label>
+      <textarea v-model="editContext" placeholder="Enter a description" rows="3" class="form-control bg-dark text-light border-light"/>
+      <button @click="editProduct" id="submit" class="mt-3 btn btn-lg text-light border-light">Edit product</button>
+      <button @click="deleteProduct" id="submit" class="mt-3 ml-3 btn btn-lg text-light border-light">Delete product</button>
+    </div>
+  </div>
 </div>
 </template>
 
@@ -25,19 +42,36 @@ export default {
   name: 'Admin',
   data() {
     return {
-      name: "",
-      dark: "",
-      context: "",
+      addName: "",
+      addDark: "1",
+      addContext: "",
+      editName: "",
+      editDark: "1",
+      editContext: "",
+      products: [],
     }
+  },
+  created() {
+    this.getProducts();
   },
   methods: {
     async addProduct() {
       try {
         await axios.post('/api/products', {
-          name: this.name,
-          dark: this.dark,
-          context: this.context
-        }); 
+          name: this.addName,
+          dark: this.addDark,
+          context: this.addContext
+        });
+        this.getProducts(); 
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async getProducts() {
+      try {
+        let response = await axios.get("/api/products");
+        this.products = response.data;
+        return true;
       } catch (error) {
         console.log(error);
       }
